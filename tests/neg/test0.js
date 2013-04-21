19076c55422b70de0b30f2bed494a1a90b945eab
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-04-20 22:21:48 -0700

    asd

---
+++ tests/neg/test0.js
@@ -0,0 +1,5 @@
+function main(){
+  var x = 0;
+  var y = x - 1;
+  assert (0 < y);
+}
---
+++ tests/neg/test1.js
@@ -0,0 +1,13 @@
+function random(){
+  var x;
+  return x;
+}
+
+function main(){
+  var y = 0;
+  var x = random();
+  y = x + y;
+  assert (0 <= y);
+}
+
+

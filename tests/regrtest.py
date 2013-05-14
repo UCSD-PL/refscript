3e234dec71a18aa56b87c8240d7b14cb9f11ea00
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-05-14 07:49:13 -0700

    update tests

--- tests/regrtest.py
+++ tests/regrtest.py
@@ -88,14 +88,13 @@ options, args = parser.parse_args()
 
 
 # ESC Tests
-# testdirs  = [ ("pos", 0), ("neg", 1)]
-# runner    = rtest.TestRunner (Config ("nanojs esc", options.opts, testdirs, logfile, options.threadcount))
-# runner.run ()
+testdirs  = [ ("pos", 0), ("neg", 1)]
+runner    = rtest.TestRunner (Config ("nanojs esc", options.opts, testdirs, logfile, options.threadcount))
+runner.run ()
 
 # Liquid Tests
 testdirs  = [ ("typed/pos", 0), ("typed/neg", 1) ]
 runner    = rtest.TestRunner (Config ("nanojs liquid", options.opts, testdirs, logfile, options.threadcount))
 runner.run ()
-
 
 # [os.system(("cd %s; ../../cleanup; cd ../" % d)) for (d,_) in testdirs]  

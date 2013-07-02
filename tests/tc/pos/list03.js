2d39b0e6ed61581c5acfa4a943392b3f0a4e5a16
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2013-07-01 21:03:31 -0700

    Adding listmap.js

--- tests/tc/pos/list03.js
+++ tests/tc/pos/list03.js
@@ -1,7 +1,7 @@
 /*@ type list[A]  {  data : A, 
                      next : list[A] | Null } */
 
-/*@ append :: forall B . (x:list[A], A) => list[A] */
+/*@ append :: forall A . (x:list[A], A) => list[A] */
 function append(x, a) {
 
     return { data: a , next: x };

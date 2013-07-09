b0ebb214731cbe9726816d3e2ba6ac7524acb130
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2013-07-09 09:36:43 -0700

    Strict Null and Undefined subtype checks

--- include/prelude.js
+++ include/prelude.js
@@ -44,17 +44,19 @@ function pos(){
 /************** Types for list Operators ******************************/
 /*************************************************************************/
 
-/* measure len :: forall A. (list [A]) => number */
+// ORIGINAL VERSION BEGIN
+    /* measure len :: forall A. (list [A]) => number */
 
-/* cons  :: forall A. (x:A, xs:list [A]) => {v: list [A] | (len v) = 1 + (len xs)}            */
-/* nil   :: forall A. () => {v: list [A] | (len v) = 0}                                       */
-/* head  :: forall A. (xs:list [A]) => A        */
-/* tail  :: forall A. (xs:list [A]) => list [A] */
-/* nth   :: forall A. (xs:list [A], {i:number| ((0 <= i) && i < (len xs))}) => A                 */
-/* empty :: forall A. (xs:list [A]) => {v:boolean | (if (Prop v) then ((len xs) = 0) else ((len xs) > 0))} */
-/* length   :: forall A. (xs:list [A]) => {v:number | ((v >= 0) && v = (len xs))}                */
-/* safehead :: forall A. ({xs:list [A] | (len xs) > 0}) => A                                     */
-/* safetail :: forall A. ({xs:list [A] | (len xs) > 0}) => {v:list [A] | (len v) = (len xs) - 1} */
+    /* cons  :: forall A. (x:A, xs:list [A]) => {v: list [A] | (len v) = 1 + (len xs)}            */
+    /* nil   :: forall A. () => {v: list [A] | (len v) = 0}                                       */
+    /* head  :: forall A. (xs:list [A]) => A        */
+    /* tail  :: forall A. (xs:list [A]) => list [A] */
+    /* nth   :: forall A. (xs:list [A], {i:number| ((0 <= i) && i < (len xs))}) => A                 */
+    /* empty :: forall A. (xs:list [A]) => {v:boolean | (if (Prop v) then ((len xs) = 0) else ((len xs) > 0))} */
+    /* length   :: forall A. (xs:list [A]) => {v:number | ((v >= 0) && v = (len xs))}                */
+    /* safehead :: forall A. ({xs:list [A] | (len xs) > 0}) => A                                     */
+    /* safetail :: forall A. ({xs:list [A] | (len xs) > 0}) => {v:list [A] | (len v) = (len xs) - 1} */
+// ORIGINAL VERSION END
 
 
 /*@ measure len :: forall A. (list [A]) => number */
@@ -77,16 +79,37 @@ function pos(){
 /************** Types for Builtin Operators ******************************/
 /*************************************************************************/
 
+// ORIGINAL VERSION BEGIN
+    /* builtin_OpLT        :: ({x:number|true}, {y:number|true}) => {v:boolean | ((Prop v) <=> (x <  y)) }*/
+    /* builtin_OpLEq       :: ({x:number|true}, {y:number|true}) => {v:boolean | ((Prop v) <=> (x <= y)) }*/
+    /* builtin_OpGT        :: ({x:number|true}, {y:number|true}) => {v:boolean | ((Prop v) <=> (x >  y)) }*/
+    /* builtin_OpGEq       :: ({x:number|true}, {y:number|true}) => {v:boolean | ((Prop v) <=> (x >= y)) }*/
+    //PV: @==@ and @===@ could be handled more precisely
+    /* builtin_OpEq        :: forall A. ({x:A|true}, {y:A|true}) => {v:boolean | ((Prop v) <=> (x = y)) } */
+    /* builtin_OpSEq       :: forall A. ({x:A|true}, {y:A|true}) => {v:boolean | ((Prop v) <=> (x = y)) } */
+    /* builtin_OpNEq       :: forall A B. ({x:A|true}, {y:B|true}) => {v:boolean | ((Prop v) <=> (x != y)) } */
+    /* builtin_OpLAnd      :: ({x:boolean|true}, {y:boolean|true}) => {v:boolean | ((Prop v) <=> ((Prop x) && (Prop y)))} */
+    /* builtin_OpLOr       :: ({x:boolean|true}, {y:boolean|true}) => {v:boolean | ((Prop v) <=> ((Prop x) || (Prop y)))}  */
+    /* builtin_OpAdd       :: ({x:number | true}, {y:number | true})  => {v:number | v = x + y} */ 
+    /* builtin_OpSub       :: ({x:number | true}, {y:number | true})  => {v:number | v = x - y} */ 
+    /* builtin_OpMul       :: (number,  number)  => number                                      */
+    /* builtin_OpDiv       :: (number,  number)  => number                                      */ 
+    /* builtin_OpMod       :: (number,  number)  => number                                      */
+    /* builtin_PrefixMinus :: ({x:number | true}) => {v:number | v = (0 - x)}                */
+    /* builtin_PrefixLNot  :: (boolean) => boolean                                     */
+    /* builtin_PrefixTypeof:: forall A. (A) => string                                  */
+    /* measure prop        :: (boolean) => bool                                        */
+// ORIGINAL VERSION END
+
 /*@ builtin_OpLT        :: ({x:number|true}, {y:number|true}) => {v:boolean | ((Prop v) <=> (x <  y)) }*/
 /*@ builtin_OpLEq       :: ({x:number|true}, {y:number|true}) => {v:boolean | ((Prop v) <=> (x <= y)) }*/
 /*@ builtin_OpGT        :: ({x:number|true}, {y:number|true}) => {v:boolean | ((Prop v) <=> (x >  y)) }*/
 /*@ builtin_OpGEq       :: ({x:number|true}, {y:number|true}) => {v:boolean | ((Prop v) <=> (x >= y)) }*/
-//PV: @==@ and @===@ could be handled more precisely
 /*@ builtin_OpEq        :: forall A. ({x:A|true}, {y:A|true}) => {v:boolean | ((Prop v) <=> (x = y)) } */
 /*@ builtin_OpSEq       :: forall A. ({x:A|true}, {y:A|true}) => {v:boolean | ((Prop v) <=> (x = y)) } */
 /*@ builtin_OpNEq       :: forall A B. ({x:A|true}, {y:B|true}) => {v:boolean | ((Prop v) <=> (x != y)) } */
-/*@ builtin_OpLAnd      :: ({x:boolean|true}, {y:boolean|true}) => {v:boolean | ((Prop v) <=> ((Prop x) && (Prop y)))} */
-/*@ builtin_OpLOr       :: ({x:boolean|true}, {y:boolean|true}) => {v:boolean | ((Prop v) <=> ((Prop x) || (Prop y)))}  */
+/*@ builtin_OpLAnd      :: (x:top, y:top) => boolean */
+/*@ builtin_OpLOr       :: (x:top, y:top) => boolean */
 /*@ builtin_OpAdd       :: ({x:number | true}, {y:number | true})  => {v:number | v = x + y} */ 
 /*@ builtin_OpSub       :: ({x:number | true}, {y:number | true})  => {v:number | v = x - y} */ 
 /*@ builtin_OpMul       :: (number,  number)  => number                                      */
@@ -96,6 +119,11 @@ function pos(){
 /*@ builtin_PrefixLNot  :: (boolean) => boolean                                     */
 /*@ builtin_PrefixTypeof:: forall A. (A) => string                                  */
 /*@ measure prop        :: (boolean) => bool                                        */
+
+
+
+
+
 
 /*************************************************************************/
 /************** Pre-Loaded Qualifiers ************************************/

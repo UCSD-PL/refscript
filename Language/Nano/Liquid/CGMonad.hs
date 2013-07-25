894426a8cd02e35d9dbdaf261726c43f28748d60
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2013-07-24 23:50:43 -0700

    Porting fixes on substJoin and minor.

--- Language/Nano/Liquid/CGMonad.hs
+++ Language/Nano/Liquid/CGMonad.hs
@@ -197,7 +197,6 @@ tag t@(TApp TBool [] r) = TApp TBool [] $ taggedReft t
 --tag   (TApp TUn   ts r) = TApp TTop [] $ foldl  mempty $ map taggedReft ts
 tag t                   = t
 
-
 taggedReft :: RType F.Reft -> F.Reft
 taggedReft (TApp TInt  [] r) = tagByNumber r 0
 taggedReft (TApp TBool [] r) = tagByNumber r 1
@@ -217,7 +216,6 @@ tagPred (F.Reft (sym, refa)) n = F.RConc pred
 addAnnot       :: (F.Symbolic x) => SourceSpan -> x -> RefType -> CGM () 
 ---------------------------------------------------------------------------------------
 addAnnot l x t = modify $ \st -> st {cg_ann = A.addAnnot l x t (cg_ann st)}
-
 
 ---------------------------------------------------------------------------------------
 envAddReturn        :: (IsLocated f)  => f -> RefType -> CGEnv -> CGEnv 
@@ -247,6 +245,8 @@ envFindTy x g = (`eSingleton` x) $ fromMaybe err $ E.envFindTy x $ renv g
 envFindReturn :: CGEnv -> RefType 
 ---------------------------------------------------------------------------------------
 envFindReturn = E.envFindReturn . renv
+
+
 
 ---------------------------------------------------------------------------------------
 -- | Fresh Templates ------------------------------------------------------------------
@@ -338,6 +338,8 @@ wellFormed       :: (IsLocated l) => l -> CGEnv -> RefType -> CGM RefType
 wellFormed l g t = do modify $ \st -> st { ws = (W g (ci l) t) : ws st }
                       return t
 
+
+
 ---------------------------------------------------------------------------------------
 -- | Generating Fresh Values ----------------------------------------------------------
 ---------------------------------------------------------------------------------------
@@ -401,7 +403,6 @@ refreshRefType = mapReftM refresh
 splitC c = splitC' c
 
 splitC' :: SubC -> CGM [FixSubC]
-
 ---------------------------------------------------------------------------------------
 -- | Function types
 ---------------------------------------------------------------------------------------
@@ -417,7 +418,6 @@ splitC' (Sub g i tf1@(TFun xt1s t1 _) tf2@(TFun xt2s t2 _))
        su         = F.mkSubst $ safeZipWith "splitC2" bSub xt1s xt2s
        bSub b1 b2 = (b_sym b1, F.eVar $ b_sym b2)
 
-
 ---------------------------------------------------------------------------------------
 -- | TAlls
 ---------------------------------------------------------------------------------------
@@ -430,7 +430,6 @@ splitC' (Sub g i (TAll α1 t1) (TAll α2 t2))
     θ   = fromList [(α2, tVar α1 :: RefType)]
     t2' = apply θ t2
 
-
 ---------------------------------------------------------------------------------------
 -- | TVars
 ---------------------------------------------------------------------------------------
@@ -439,7 +438,6 @@ splitC' (Sub g i t1@(TVar α1 _) t2@(TVar α2 _))
   = return $ bsplitC g i t1 t2
   | otherwise
   = errorstar "UNEXPECTED CRASH in splitC"
-
 
 ---------------------------------------------------------------------------------------
 -- | Unions
@@ -464,7 +462,6 @@ splitC' (Sub g i t1 t2@(TApp TUn t2s _))
         cs'   <- unionFixSubs g i [t1] t2s
         return $ {-cs ++ -} cs'
 
-
 ---------------------------------------------------------------------------------------
 -- | Type definitions
 ---------------------------------------------------------------------------------------
@@ -486,7 +483,6 @@ splitC' (Sub g i t1 t2@(TApp (TDef _) _ _ ))
   = do  env <- cg_tdefs <$> get
         splitC $ Sub g i t1 (unfoldTDefSafe t2 env)
 
-
 ---------------------------------------------------------------------------------------
 -- | Rest of TApp
 ---------------------------------------------------------------------------------------
@@ -496,7 +492,6 @@ splitC' (Sub g i t1@(TApp _ t1s _) t2@(TApp _ t2s _))
                                     (printf "splitC4: %s - %s" (ppshow t1) (ppshow t2)) 
                                     (Sub g i) t1s t2s
        return $ cs ++ cs'
-
 
 ---------------------------------------------------------------------------------------
 -- | Objects 
@@ -535,7 +530,6 @@ bsplitC g ci t1 t2
     r1 = rTypeSortedReft t1
     r2 = rTypeSortedReft t2
 
-
 ---------------------------------------------------------------------------------------
 unionFixSubs :: CGEnv -> Cinfo -> [RefType] -> [RefType] -> CGM [FixSubC]
 ---------------------------------------------------------------------------------------
@@ -544,7 +538,6 @@ unionFixSubs g i t1s t2s = concatMapM mkSub =<< matchTypes g i t1s t2s
     mkSub (x,y)     = {- T.trace (printf "UnionFixSubs %s: %s <: %s" 
                           (ppshow i) (ppshow x) (ppshow y)) $ -}
                       splitC $ Sub g i x y
-
 
 ---------------------------------------------------------------------------------------
 matchTypes :: CGEnv -> Cinfo -> [RefType] -> [RefType] -> CGM [(RefType, RefType)]
@@ -561,7 +554,6 @@ matchTypes g i t1s t2s =
                         Just y -> ((tag x, tag y):acc, L.delete y ys)
                         _      -> ((tag x, tag $ fal x):acc, ys)
     fal t           = (ofType $ toType t) `strengthen` (F.predReft F.PFalse)
-
 
 
 

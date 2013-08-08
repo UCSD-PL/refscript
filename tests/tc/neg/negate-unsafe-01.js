20270265e372fbddfdbac87d368ad40c219ac386
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2013-08-08 13:44:06 -0700

    Adding verbosity option
    - Fixed some negate tests

--- tests/tc/neg/negate-unsafe-01.js
+++ tests/tc/neg/negate-unsafe-01.js
@@ -1,8 +1,5 @@
 /*@ negate :: (number + boolean) => number + boolean */
 function negate(x) {
-//Int   -> 0
-//Bool  -> 1
-//Revert when strings are supported   
   if (typeof(x) == 0) {
     return 0-x;
   }

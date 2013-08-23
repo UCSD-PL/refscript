3e10e52d5bd0b2f2eacb132a319303d7ce23320d
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2013-08-23 10:28:20 -0700

    WORK IN PROGRESS:
    - Adding fixbase for upcasts

--- tests/liquid/pos/negate.js
+++ tests/liquid/pos/negate.js
@@ -1,4 +1,8 @@
-/*@ negate :: (x: number + boolean) => { v: number + boolean | true } */
+/*@ qualif Tag1(v:a, w:b)               : ttag([v])  = ttag([w])                 */
+/*@ qualif Tag2(v:a, w:b)               : ttag([v]) != ttag([w])                 */
+
+/*@ negate :: ({x: number + boolean | true }) => 
+    { v: number + boolean | (ttag(v) = ttag(x)) } */
 function negate(x) {
   if (typeof(x) == "number") {
     return 0 - x;

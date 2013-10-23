91126c267bc0feb909cb695933da5ea63ce28bc4
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2013-10-23 02:06:24 +0100

    Adding positive while-loops example - not working

---
+++ tests/liquid/pos/loops/while-03.js
@@ -0,0 +1,25 @@
+
+/*@ qualif Eq1(v : number, w: number): (v = w ) */
+/*@ qualif Eq2(v : number, w: number): (v = w + 1) */
+/*@ qualif Eq3(v : number, w: number): (1 = 0 + 1) */
+/*@ qualif Ineq1(v : number ): (v <= 5) */
+/*@ qualif Ineq2(v : number ): (v <= 4) */
+/*@ qualif Ineq3(v : number ): (4 <= v) */
+/*@ qualif EEq1(v : number ): (v = 5) */
+/*@ qualif EEq2(v : number ): (v = 4) */
+
+/*@ foo :: () => { number | v = 4 } */ 
+
+function foo() {
+
+  var x = 0;
+  var i = 1; 
+
+  while (i < 5) {
+     x = i;
+     i = i + 1; 
+  }
+
+  return x;
+
+}

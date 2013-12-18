8ed4671f7ad978aad1d8a827024ea1377a21c797
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-12-17 20:12:47 -0800

    last 8 tests. what to do about array length...

---
+++ tests/liquid/todo/obj-01.js
@@ -0,0 +1,26 @@
+/*@ qualif PLusOne(v:number, w: number)     : v = w + 1                            */
+
+/*@ inc :: (number) => number  */
+function inc(n) {
+  return n + 1;
+}
+
+// DO THIS WITHOUT THE ANNOTATION -- i.e. INFERRING the ANNOTATION
+/* gobj :: { a: {number | v = 5}
+           , b: string
+           , f: (x:number) => {number | v = x + 1}
+           } 
+ */
+var gobj = {
+  a: 5,
+  b: "String",
+  f: inc
+}
+
+/*@ foo :: () => { number | v = 6 } */
+function foo () {
+  
+  var ff = gobj.f;
+  return ff(gobj.a);
+
+}

c92db1aff4a4a0a19761a3aba402c8098199cadc
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-12-20 14:51:11 -0800

    Added code for qualgen, lets see if it works...

--- src/Language/Nano/Liquid/Qualifiers.hs
+++ src/Language/Nano/Liquid/Qualifiers.hs
@@ -1,8 +1,47 @@
 module Language.Nano.Liquid.Qualifiers (nanoQualifiers) where
 
-import qualified Language.Fixpoint.Types as F
+import Language.Fixpoint.Errors
+import Language.Fixpoint.Types hiding (quals) 
 import Language.Nano.Typecheck.Types
 import Language.Nano.Liquid.Types
+import Language.Nano.Env
+import Language.Nano.Errors
+import Language.Nano.Types
+import Language.ECMAScript3.Syntax
+import Control.Applicative      ((<$>))
+import Data.List                (delete, nub)
+import Data.Maybe               (fromMaybe)
 
-nanoQualifiers :: NanoRefType -> [F.Qualifier]
-nanoQualifiers = quals -- undefined
+
+nanoQualifiers         :: NanoRefType -> [Qualifier]
+nanoQualifiers p       = quals p ++ nanoQualifiers' p
+
+nanoQualifiers'        :: NanoRefType -> [Qualifier]
+nanoQualifiers' p      = concatMap (refTypeQualifiers γ0) $ envToList $ defs p 
+  where
+    γ0                 = envSEnv $ envMap rTypeSort $ specs p
+
+refTypeQualifiers γ0 (l, t) = efoldRType rTypeSort addQs γ0 [] t 
+  where
+    addQs γ t qs            = (mkQuals l γ t) ++ qs
+
+mkQuals l γ t      = [ mkQual l γ v so pa | let (RR so (Reft (v, ras))) = rTypeSortedReft t 
+                                          , RConc p                    <- ras                 
+                                          , pa                         <- atoms p
+                     ]
+
+mkQual l γ v so p = Q "Auto" ((v, so) : yts) $ subst θ p
+  where 
+    yts           = [(y, lookupSort l x γ) | (x, y) <- xys ]
+    θ             = mkSubst [(x, eVar y)   | (x, y) <- xys]
+    xys           = zipWith (\x i -> (x, stringSymbol ("~A" ++ show i))) xs [0..] 
+    xs            = delete v $ orderedFreeVars γ p
+
+lookupSort l  x γ = fromMaybe err $ lookupSEnv x γ 
+  where 
+    err           = die $ bug (srcPos l) $ "Unbound variable " ++ show x ++ " in specification for " ++ show (unId l)
+
+orderedFreeVars γ = nub . filter (`memberSEnv` γ) . syms 
+
+atoms (PAnd ps)   = concatMap atoms ps
+atoms p           = [p]

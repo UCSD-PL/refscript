5e0ba9ee48c85d5595fef6d41408023404b8aa7a
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-12-21 10:24:57 -0800

    asdasd

--- tests/liquid/pos/simple/qualgen-00.js
+++ tests/liquid/pos/simple/qualgen-00.js
@@ -1,9 +1,12 @@
 
-/*@ arr :: [ { number |( (v > 0)  && (v < 3))} ] */
-var arr =  [1, 2];
+/*@ z :: {number | (0 < v && v < 3)} */
+var junk = 2;
+
+var arr  =  [1, 2];
 
 /*@ foo :: () => { number | v < 4 } */
 function foo() {
+  arr[0] = 2;
   return arr[0] + 1;
 }
 

5e03eadf29e2e6a757a2c337e30506f34447f29b
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2013-12-29 04:24:32 +0200

    Minor fix

--- src/Language/Nano/ESC/Types.hs
+++ src/Language/Nano/ESC/Types.hs
@@ -38,7 +38,7 @@ import           Text.PrettyPrint.HughesPJ
 ---------------------------------------------------------------------
 
 parseNanoFromFile f 
-  = do (s,_) <- parseJavaScriptFromFile f
+  = do s     <- parseJavaScriptFromFile f
        return $ fromMaybe err (mkNano s)
     where
        err    = errorstar $ "Invalid Input File: " ++ f

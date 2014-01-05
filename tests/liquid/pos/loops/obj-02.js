fdfd0b6077b2bd40d05703a99bf3f20f9d1dfcb8
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-01-04 18:34:18 -0800

    need to add distinct -- Error/Bug -- constructors to language.fixpoint.errors to distinguish between the two cases, e.g. errorArgMismatch

--- tests/liquid/pos/loops/obj-02.js
+++ tests/liquid/pos/loops/obj-02.js
@@ -1,29 +1,18 @@
+// related: tests/liquid/pos/loops/for-05.js 
 
+/*@ qualif Poo(v:number): v = -1 */
 /*@ qualif Poo(v:number, i:number): v = i - 1 */
-/*@ qualif Poo(v:number, i:number): v = i */
 /*@ qualif Poo(v:number): v < 5 */
-/*@ qualif Poo(v:number): v < 4 */
-/*@ qualif Poo(v:number): v <= 5 */
-/*@ qualif Poo(v:number): v <= 4 */
+
 
 /*@ foo :: () => { a: { number | 4 = v } } */ 
 function foo() {
-  var x = { a: 1 };
+  var x = { a: (-1) }; //need silly singleton v = -1 qualifier as we generate a template for this x. Ugh.
 
   for (var i = 0; i < 5; i++) {
      x = { a: i };
   }
 
   return x;
-}
-
-// PROVE THIS FIRST!
-/*@ bar :: () => { number | 4 = v } */ 
-function bar(){
-  var z = 1;
-  for (var i = 0; i < 5; i++) {
-     z = i;
-  }
-  return z;
 }
 

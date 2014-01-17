aa7f15ecc2b6b87820009dd2b94ffebafdc8f8b0
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2014-01-16 17:47:46 -0800

    Adding parts of resistor

---
+++ tests/strobe/gadgets/resistor-part-01.js
@@ -0,0 +1,19 @@
+
+/*@ containsErroneousNonZeroDigits :: (inputStr: string, zerosStartAtPosition: number) => { boolean | true } */
+function containsErroneousNonZeroDigits(inputStr, zerosStartAtPosition) {
+    var length = inputStr.length;
+
+    //if (length <= zerosStartAtPosition) {
+    //    return (false);
+    //}
+
+    //for (var i = zerosStartAtPosition; i < length; i++) {
+    //    if (inputStr.charAt(i) != "0") {
+    //      //TODO
+    //      //return (true);
+    //    }
+    //}
+
+    return (false);
+}
+

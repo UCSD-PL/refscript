f0025b959146d225eb6410283482a8b4a89ac08b
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-02-05 17:44:03 -0800

    refreshing heapless, zog

---
+++ tests/liquid/neg/objects/obj-07-hide.js
@@ -0,0 +1,17 @@
+/*@ gobj :: { a: number 
+            , b: string
+            , oo: { m : number }
+            } 
+ */
+var gobj = {
+  a: 5,
+  b: "String",
+  oo: { n: 6 }
+}
+
+/*@ foo :: ({ number | true } ) => {  } */
+function foo (n) {
+  return gobj.oo;
+}
+
+

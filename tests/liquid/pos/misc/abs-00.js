414cfe9617937ce59196168e23e2fb87c7d6fb4a
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-02-05 21:27:11 -0800

    asdaSD

--- tests/liquid/pos/misc/abs-00.js
+++ tests/liquid/pos/misc/abs-00.js
@@ -1,11 +1,11 @@
 
 /*@ abs :: (x:number) => {v:number | v >= x} */ 
 function abs(x){
-  var res /*@  number */  = 0;
+  var res = 0;
   if (x > 0) {
     res = x;
   } else {
-    res = -x;
+    res = (0 - x);
   };
   assert(res >= 0);
   return res;

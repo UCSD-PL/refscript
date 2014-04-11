b2397cab3cc3d5a8ec3864def416a330cbed2a1f
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2014-04-11 16:35:57 -0700

    Making typescript to refscript translation optional.
      - Fixed README
      - Fixed Setup.hs
      - Added tsc binaries.

--- Setup.hs
+++ Setup.hs
@@ -4,22 +4,27 @@ import Distribution.PackageDescription
 import Distribution.Simple
 import Distribution.Simple.LocalBuildInfo
 import Distribution.Simple.Setup
-import System.Posix.Env
+import System.Posix.Env hiding (getEnvironment)
+import System.Environment (getEnvironment)
 import System.Process
 import System.Exit
+import Data.List (find)
 
 main         = defaultMainWithHooks fixHooks 
   where 
-    fixHooks = simpleUserHooks { postInst = buildAndCopyFixpoint } 
+    fixHooks = simpleUserHooks { postInst = copyExts } 
    
-buildAndCopyFixpoint _ _ pkg lbi 
+copyExts _ _ pkg lbi 
   = do putStrLn $ "Post Install - copy TS binaries into PATH: " ++ show binDir
-  -- Compile and copy TypeScript executables
+       env     <- getEnvironment 
+       let tscR = fromMaybe lTscRoot $ fmap snd $ find ((== "TSC_ROOT") . fst) env
+       putStrLn $ "Using '" ++ tscR ++ "' for `tsc` sources." 
+       -- Compile and copy TypeScript executables
        executeShellCommand   "chmod a+x ../typescript/bin/tsc"
-       mapM_ executeShellCommand (map cpFile tscBins)
-  -- Compile and copy TypeScript the prelude
+       mapM_ executeShellCommand (map cpFile $ tscBins tscR)
+       -- Compile and copy TypeScript's prelude
        executeShellCommand $ "tsc --nano " ++ preludeSrc
-       executeShellCommand ("cp " ++ preludeTgt ++ " " ++ dataDir ++ preludeTgt)
+       executeShellCommand $ "cp " ++ preludeTgt ++ " " ++ dataDir ++ preludeTgt
   where 
     allDirs     = absoluteInstallDirs pkg lbi NoCopyDest
     withSpaces  = concatMap $ (++) " "
@@ -27,12 +32,14 @@ buildAndCopyFixpoint _ _ pkg lbi
     dataDir     = datadir allDirs ++ "/"
 
     cpFile f    = withSpaces ["cp", f, binDir]
-    tscBins     = [ "../typescript/bin/tsc",
-                    "../typescript/built/local/tsc.js",
-                    "../typescript/typings/lib.d.ts",
-                    "../typescript/typings/jquery.d.ts",
-                    "../typescript/typings/winjs.d.ts",
-                    "../typescript/typings/winrt.d.ts" ]
+    lTscRoot    = "ext/tsc-bin"
+    tscBins r   = map (r ++) ["/bin/tsc",
+                              "/built/local/tsc.js",
+                              "/typings/lib.d.ts",
+                              "/typings/jquery.d.ts",
+                              "/typings/winjs.d.ts",
+                              "/typings/winrt.d.ts" ]
+
     preludeSrc  = "include/prelude.ts"
     preludeTgt  = "include/prelude.json"
     flags       = configConfigurationsFlags $ configFlags lbi

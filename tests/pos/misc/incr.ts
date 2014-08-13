e35164fb8c1cf9d4c5d6f333311684345e8cf7a9
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-08-12 21:54:59 -0700

    more tests for unary assignments

--- tests/pos/misc/incr.ts
+++ tests/pos/misc/incr.ts
@@ -1,15 +1,21 @@
 
-/*@ incr :: (x:number) => {number|v = x + 1} */
-function incr(x:number):number{
+/*@ nincr :: (x:number) => {number|v = x} */
+function nincr(x:number):number{
   return x++;
 }
+
+/*@ incr :: (x:number) => {number|v = x + 1} */
+function incr(x:number):number{
+  return ++x;
+}
+
+
 
 /*@ incrBy :: (x:number, k:number) => {number|v = x + k} */
 function incrBy(x:number, k:number):number{
   var z = x + k;
   return z;
 }
-
 
 /*@ bar :: (x:number) => {number|v = x + 4} */
 function bar(x:number):number{

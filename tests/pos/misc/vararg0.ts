5c17ab20c3804a8bb16f47735f85f48e44e9f788
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-08-18 10:00:53 -0700

    adding vararg tests

---
+++ tests/pos/misc/vararg0.ts
@@ -0,0 +1,17 @@
+
+
+/*@ buildName :: /\ (firstName: string, lastName: undefined) => string
+                 /\ (firstName: string, lastName: string   ) => string
+ */
+
+function buildName(firstName: string, lastName?: string) {
+    if (lastName)
+        return firstName + " " + lastName;
+    else
+        return firstName;
+}
+
+var result1 = buildName("Bob");                  //works correctly now
+var result3 = buildName("Bob", "Adams");         //ah, just right
+
+// NEG : var result2 = buildName("Bob", "Adams", "Sr.");  //error, too many parameters

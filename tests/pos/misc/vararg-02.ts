a6fea8e72b1670463b554c626ef1e74a18c0bd87
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-08-25 12:06:47 -0700

    done with varargs maybe

--- tests/pos/misc/vararg-02.ts
+++ tests/pos/misc/vararg-02.ts
@@ -13,4 +13,3 @@ function buildName(firstName: string, lastName?: string) {
 
 var result1 = buildName("Bob");                  //works correctly now
 var result3 = buildName("Bob", "Adams");         //ah, just right
-var result2 = buildName("Bob", "Adams", "Sr.");  //error, too many parameters

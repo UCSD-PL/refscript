4660da124d2d0f547dd72b53a45bf800bbaabba8
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2014-09-02 19:07:13 -0700

    Adding forin examples.

---
+++ tests/pos/simple/forin-00.ts
@@ -0,0 +1,9 @@
+
+/*@ values :: forall T . (map: {[k:string]: { number | v > 0 }}) => { number | v > 0 } */
+function values<T>(map:{[k:string]:T}):T[] {
+  var values:T[] = [];
+  
+  for (var key in map) values.push(map[key]);
+  
+  return values;
+};

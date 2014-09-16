6382482f1d5454fb5555fb0985ce43790163f158
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-09-16 13:32:41 -0400

    adding test for supporting truthy values in ternary choice

--- tests/pos/operators/choice.ts
+++ tests/pos/operators/choice.ts
@@ -1,3 +1,16 @@
+//THIS IS FINE
+
+/*@ bob :: (x:number) => {v:number | v != 0} */
+function bob(x:number):number {
+    if (x) {
+	return x;
+    } else { 
+	return (x + 1);
+    }
+}
+
+// BUT this is not -- and should be treated like the above...
+
 /*@ main :: (x:number) => {v:number | v != 0} */
 function main(x:number):number {
   return x ? x : x + 1;

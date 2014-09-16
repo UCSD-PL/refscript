bd2f651185bb72c893c107e99d342af27d8a3b46
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-09-16 11:19:07 -0400

    sigh. MUST report source locations for ALL errors, e.g. malformed alias applications

---
+++ tests/neg/typealias/talias-06.ts
@@ -0,0 +1,15 @@
+
+/*@ alias iArray[T]    = #Array[#Immutable, T]        */
+
+// TODO: *******MUST****** report error locations for malformed type-aliases!!!! 
+
+/*@ baz :: forall T. (#iArray) => {v:number | true} */
+function baz<T>(array: T[]): number {
+
+  var i = 0, 
+      n = array.length - 1, 
+      p0, p1 = array[0], 
+      pairs = new Array(n < 0 ? 0 : n);
+ 
+  return i;
+}

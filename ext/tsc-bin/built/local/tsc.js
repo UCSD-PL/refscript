828b04b4892c4da481838a668631314928f59eca
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2014-09-16 16:14:25 -0700

    Fixing off by one error in TS sourcespan

--- ext/tsc-bin/built/local/tsc.js
+++ ext/tsc-bin/built/local/tsc.js
@@ -60584,7 +60584,7 @@ var TypeScript;
             this.column = column;
         }
         FPSrcPos.prototype.toObject = function () {
-            return [this.name, this.line, this.column];
+            return [this.name, this.line + 1, this.column + 1];
         };
         return FPSrcPos;
     })();

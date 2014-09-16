19bcefe51331c6b78ab0ff1ac6cc5a336983c4a7
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-09-16 16:16:46 -0400

    adding undef test

---
+++ tests/pos/unions/undef-00.ts
@@ -0,0 +1,19 @@
+
+/*@ foo :: forall T. (T, {v:number | v > 0}) => T + undefined */
+function foo<T>(x:T, n:number) {
+    var i = n;
+
+    /*@ a :: T + undefined */
+    var a = undefined;
+
+    while (0 < i){
+	if (i > 100) {
+            a = x;
+	} else {
+            a = undefined;
+	}
+	i--;
+    }
+    
+    return a;
+}

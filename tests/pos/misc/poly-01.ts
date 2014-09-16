54d550fda27455a778262b5fe6157aa37f389c56
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-09-16 17:42:21 -0400

    test for doing poly-inst for HOF arguments

---
+++ tests/pos/misc/poly-01.ts
@@ -0,0 +1,16 @@
+
+
+/*@  idd :: forall T. (x:T) => T */
+function idd<A>(x:A):A {
+    return x;
+}
+
+/*@ bar :: forall A. (A, (x:A) => A) => A */
+function bar(x:any, f:any){
+    return f(x);
+}
+
+/*@ main :: (number) => {number | true} */
+function main(x){
+    return bar(x, idd);
+}

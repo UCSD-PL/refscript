a2a61f19ee2013606106cf8358472c6bedb00319
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2014-09-24 10:32:54 -0700

    Disallowing refinements at functions in parsing
      - Functions should NOT be treated as callable objects.

--- tests/neg/simple/fun-06.ts
+++ tests/neg/simple/fun-06.ts
@@ -1,4 +1,4 @@
-/*@ foo1 :: (x:number) => () => { undefined | true } */
+/*@ foo1 :: (x:number) =>{  () => { undefined | true } } */
 function foo1(x:number) {
     return function() { return undefined }
 }

b967b16d940d15bcea475e8aa8a91828a19f2992
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-09-25 08:26:42 -0400

    see tests/pos/simple/fixme.ts for issue

--- tests/pos/simple/fixme.ts
+++ tests/pos/simple/fixme.ts
@@ -1,20 +1,7 @@
 
-interface Point<A1> {
-  x:A1;
-  y:A1;
-}
+/*@ alias idx[a]    = {v: number | v < a }  */
 
-interface ColorPoint<A2,C2> extends Point<A2> { 
-  c:C2;
-}
-
-/*@ foo :: forall M A3 . (#Point<M,A3>) => void */
-function foo<Z>(p: Point<Z>) {
-    return;
-}
-
-/*@ p :: #ColorPoint[#Immutable,number,string] */
-var p: ColorPoint<number, string>;
-
-foo(p);
-
+/*@ permute :: (x:number, y:idx[x]) => number */
+function permute(x, y):number {
+    return 0;
+} 

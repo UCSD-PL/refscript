f3b9c82c45f3a05b8fdf5fb5cff14a9cb85f5835
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-09-26 10:16:40 -0400

    I think tests pass now, time to merge

--- tests/pos/typealias/talias-04.ts
+++ tests/pos/typealias/talias-04.ts
@@ -3,16 +3,14 @@
 /*@ alias Nat          = {number | gt(v, 0) }      */
 /*@ alias IArray<T>    = Array<Immutable, T>       */
 /*@ alias IArrayN<T,n> = {v:IArray<T> | len v = n} */
-
+/*@ alias FourNats     = IArrayN<Nat,4> */
 
 /*@ a1 :: IArray<nat>    */
 var a1 = [0, 10, 20, 30];
 
-// WTF does this work?
-/* alias CremaCoffee  = IArrayN<Nat,4> */
-/* a2 :: CremaCoffee */
-
-// BUT this does not?
-/*@ a2 :: IArrayN<Nat,4> */
+/*@ a2 :: FourNats       */
 var a2 = [0, 10, 20, 30];
+
+/*@ a3 :: IArrayN<Nat,4> */
+var a3 = [0, 10, 20, 30];
 

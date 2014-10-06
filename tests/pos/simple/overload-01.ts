cf035d5a3335539557c49878bd49fe382922632e
Panagiotis Vekris <panagosg7@users.noreply.github.com>
2014-10-06 14:39:22 -0700

    Update overload-01.ts

--- tests/pos/simple/overload-01.ts
+++ tests/pos/simple/overload-01.ts
@@ -56,24 +56,24 @@ class A {
 	classfoo(x: string): string;
 	classfoo(x: boolean): boolean;
 	classfoo(x: any): any {
-    if (typeof x === "number") return 1;
-    else if (typeof x === "string") return "a";
-    else return true;
+    		if (typeof x === "number") return 1;
+    		else if (typeof x === "string") return "a";
+    		else return true;
 	}
 
 	static sssss(x: number): number;
 	static sssss(x: string): string;
 	static sssss(x: boolean): boolean;
 	static sssss(x: any): any {
-    if (typeof x === "number") return 1;
-    else if (typeof x === "string") return "a";
-    else return true;
+    		if (typeof x === "number") return 1;
+    		else if (typeof x === "string") return "a";
+    		else return true;
 	}
 
-  BBBBBB: (x: number) => number = 
-    function (x: number) 
-    /*@ <anonymous> (number) => number */
-    { return 1; };
+	BBBBBB: (x: number) => number = 
+	    function (x: number) 
+	    /*@ <anonymous> (number) => number */
+	    { return 1; };
 }
 
 

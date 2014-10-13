af071253bbe8370577287287e5122cbf0420c376
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2014-10-13 11:36:38 -0700

    Typing fb/min-index-02.ts

--- tests/pos/fb/min-index-02.ts
+++ tests/pos/fb/min-index-02.ts
@@ -1,16 +1,15 @@
-function reduce(me, callback, init){
-  var res = init;
-  for (var i = 0; i < me.length; i++){
-    res = callback(res, me[i], i);
-  }
-  return res;
-}
 
-// Whats a good signature for "reduce", to get the below to check?
- 
+/*@ alias IArray<T> = Array<Immutable, T> */
+
+/*@ minIndex :: (IArray<number>) => {v:number | true} */
 function minIndex(arr){
-  function body(min, cur, i){ 
-    return cur < arr[min] ? i : min 
+  function body(min: number, cur: number, i: number) { 
+
+    if (min < arr.length) {
+      return cur < arr[min] ? i : min 
+    }
+    return min;
+
   }; 
   return arr.reduce(body, 0);
 }

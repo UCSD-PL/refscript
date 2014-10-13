50563b607b65278ba5c65685cbe88ab888acd833
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-10-13 17:14:56 -0400

    issue has to do with quals I think...

--- tests/pos/fb/min-index-01.ts
+++ tests/pos/fb/min-index-01.ts
@@ -1,26 +1,11 @@
 /*@ alias IArray<T> = Array<Immutable, T> */
 
-/*  qualif GT0(n: number)        : 0 <= n      */
-/*  qualif Len(v: a, n: number)  : n < (len v) */
-/*  qualif Len(v: a)             : (len v) > 0 */
-/*  qualif Laa(v: a, n: number)  : (len v > 0) => ((0 <= n) && (n < (len v))) */ 
+/*@ qualif Len(v: a, arr: b)     : v < (len arr) */
 
-/*@ predicate inbounds(v, a) = ((0 <= v) && (v < (len a))) */ 
+/*@ alias index<a> = {v:number | 0 <= v && v < len a} */ 
 
 
-/*  reduce :: forall T A . (IArray<T>, callback: (x: A, y: T, n: number) => A, init:A) => A */
-
-/*@ reduce :: ( me      : { IArray<number> | (len v) > 0 }
-              , callback: (x: { number | inbounds(v,me)}, y: number, n: { number | inbounds(v,me)} ) => { number | inbounds(v,me)}
-              , init    : { number | inbounds(v,me)} ) 
-              => { number | inbounds(v,me)} 
- */
-
-/*  reduce :: ( me      : IArray<number>
-              , callback: (x: number, y: number, n: number) => number
-              , init    : number) 
-              => number
- */
+/*@  reduce :: forall T A . (arr:IArray<T>, callback: (x: A, y: T, index<arr>) => A, init:A) => A */
 function reduce<T,A>(me: T[], callback:(x: A, y: T, n: number) => A, init:A): A {
   var res = init;
   for (var i = 0; i < me.length; i++){
@@ -33,6 +18,8 @@ function reduce<T,A>(me: T[], callback:(x: A, y: T, n: number) => A, init:A): A 
 function minIndex(arr: number[]): number {
 
   function body(min: number, cur: number, i: number) { 
+      assert(0 <= min);
+      assert(min < arr.length);
       return cur < arr[min] ? i : min; 
   }; 
 

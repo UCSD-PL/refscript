26b3badc2bcd8ebcbb6795eebe51d291464b5508
Benjamin Cosman <bcosman@caltech.edu>
2014-10-22 13:23:03 -0700

    Minor tests cleanup

--- tests/pos/misc/bitwise-00.ts
+++ tests/pos/misc/bitwise-00.ts
@@ -1,13 +1,9 @@
-
 /*@ randomN :: (n:{number | 0 <= n}) => {v:number | 0 <= v && v <= n} */
-
-
 function randomN(n:number){
     var r = Math.random() * n;
-    // this next line triggers a crash. I'm fine to just SUPPORT the operator, 
-    // no need for any fancy semantics, since the assume ensures that typechecking passes.
+    // We just SUPPORT the operator with no fancy semantics;
+    // the assume ensures that typechecking passes.
     r = r | 0;
     assume (0 <= r && r <= n);
     return r;
 }
-

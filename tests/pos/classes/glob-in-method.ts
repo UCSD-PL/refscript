1db10d4c92a6e8b1e292a4917bfb07f451ec7ac4
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2014-10-28 15:49:38 -0700

    Adding test

---
+++ tests/pos/classes/glob-in-method.ts
@@ -0,0 +1,14 @@
+class Blah {
+  /*@ new () => void */
+  constructor() { }
+
+  public state = 3;
+
+  /*@ foo : () : { void | true } */
+  public foo() {
+    /*@ val :: number + null */
+    var val = null;
+    if (this.state === 42) val = 7;
+  }
+}
+

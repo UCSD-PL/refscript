7fec857d886dff15a8d8ac8977e9e704248158a6
Benjamin Cosman <bcosman@caltech.edu>
2015-01-05 13:38:33 -0800

    Tests cleanup

--- tests/neg/arrays/infer-00a.ts
+++ tests/neg/arrays/infer-00a.ts
@@ -2,7 +2,7 @@ var garr : number[] = [1,2,3,4];
 
 /*@ foo :: () => void */
 function foo() : void {
-  garr[2] = 0;
+  garr[2] = 10;
 }
 
 /*@ bar :: ({number | (0 <= v && v <= 4)}) => {number | v > 0} */
---
+++ tests/neg/arrays/infer-00b.ts
@@ -0,0 +1,12 @@
+var garr : number[] = [1,2,3,4];
+
+/*@ foo :: () => void */
+function foo() : void {
+  garr[2] = 0;
+}
+
+/*@ bar :: ({number | (0 <= v && v <= 3)}) => {number | v > 0} */
+function bar(n : number) : number{
+  var z : number = garr[n];
+  return z;
+}

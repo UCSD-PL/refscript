667d66aa58581b4ea801cfe6ae299ea277e82e1a
Benjamin Cosman <bcosman@caltech.edu>
2015-01-05 16:06:56 -0800

    Tests cleanup

--- tests/neg/typealias/talias-02.ts
+++ tests/neg/typealias/talias-02.ts
@@ -1,7 +1,8 @@
-/*@ predicate gt x y  = x < y          */
-/*@ alias gnat[A,x]   = {A | (gt v x)}  */
+/*@ predicate gt x y  = x < y            */
+/*@ alias gnat[A,x]   = {A | gt(v, x)}   */
 /*@ alias nat         = #gnat[number, 0] */
 
-// thus `nat` is just an alias for: {number | (gt v 0)} 
+// thus `#nat` is just an alias for: {number | gt(v, 0)} 
+
 /*@ z :: #nat */
-var z = 12;
+var z : number = 12;

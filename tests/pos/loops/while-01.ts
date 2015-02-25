394a389a1e848b77bf37b89c30e48fca6028dcca
Benjamin Cosman <bcosman@caltech.edu>
2015-02-24 16:27:20 -0800

    tests cleanup

--- tests/pos/loops/while-01.ts
+++ tests/pos/loops/while-01.ts
@@ -2,11 +2,10 @@
 /*@ qualif Ineq(v : number ): (v <= 6) */
 
 /*@ loop :: () => { number | v = 6 } */
-function loop() : number{  
-	var x :number= 1;
-	while (x <= 5) {
-		x = x + 1; 
-	}
-	return x;
+function loop() {
+  var x = 1;
+  while (x <= 5) {
+    x = x + 1;
+  }
+  return x;
 }
-

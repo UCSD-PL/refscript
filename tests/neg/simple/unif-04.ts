8cd4bb703db8b300d3281b86fa97e8332db0a1f5
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2015-03-02 14:26:21 -0800

    Wrapping unification errors in deadcasts

---
+++ tests/neg/simple/unif-04.ts
@@ -0,0 +1,10 @@
+//adapted from transducers
+class Foo<T> { constructor(){} }
+/*@ reduce :: /\            (Foo<Immutable, boolean>, string) => {void | true} 
+              /\ forall T . (Foo<Immutable, T>,       number) => {void | true} */
+function reduce(xf, coll) {
+    if (typeof coll === "number") {
+        /*@ xxf :: Foo<Immutable, boolean> */
+        var xxf = xf;
+    }
+}

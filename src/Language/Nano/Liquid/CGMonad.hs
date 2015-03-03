e76aa43e07b1c9a98d0e304423e8240e6311dee7
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2015-03-02 21:30:06 -0800

    Restoring object invariants

--- src/Language/Nano/Liquid/CGMonad.hs
+++ src/Language/Nano/Liquid/CGMonad.hs
@@ -459,6 +459,7 @@ addInvariant g t
     -- | typeof 
     typeof t@(TApp tc _ o)  i = maybe t (strengthenOp t o . rTypeReft . val) $ HM.lookup tc i
     typeof t@(TRef _ _ _) _   = t `strengthen` F.Reft ((vv t,) [ F.RConc $ typeofExpr $ F.symbol "object" ])
+    typeof t@(TCons _ _ _) _  = t `strengthen` F.Reft ((vv t,) [ F.RConc $ typeofExpr $ F.symbol "object" ])
     typeof   (TFun a b c _) _ = TFun a b c typeofReft
     typeof t                _ = t
     -- | Truthy

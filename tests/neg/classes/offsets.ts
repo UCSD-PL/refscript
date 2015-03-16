510c38cfb406cd1fa4100b9cf20d658ed46eb567
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2015-03-16 14:45:57 -0700

    Adding tests

---
+++ tests/neg/classes/offsets.ts
@@ -0,0 +1,14 @@
+
+//adapted from navier-stokes
+
+class Foo {
+    /*@ x : [Immutable] number */
+    x;
+    /*@ new (x:number) => {Foo<M> | offset(v,"x") ~~ x} */
+    constructor(x) { this.x = x }
+}
+
+/*@ foo :: ({Foo<Immutable> | offset(v,"x") ~~ 5}) => {number | v = 1} */
+function foo(z) {
+    return z.x - 5;
+}

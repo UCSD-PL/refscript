9280a94f60bccc3a45b5e7d4b8f9e7e15c781450
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2015-10-22 21:05:41 -0700

    Adding prelude building script in `include`

---
+++ include/build-prelude.ts
@@ -0,0 +1,30 @@
+
+declare function require(x: string): any;
+declare var __dirname: string;
+
+var exec = require('child_process').exec;
+var path = require('path');
+
+var outputFile = path.join(__dirname, "./full-prelude.d.ts");
+
+var files = [
+    "./rsc/prims.d.ts",
+    "./rsc/mutability.d.ts",
+    "./rsc/aliases.d.ts",
+    "./rsc/qualifiers.d.ts",
+    "./rsc/measures.d.ts",
+    "./ambient/undefined.d.ts",
+    "./ambient/object.d.ts",
+    "./ambient/array.d.ts",
+    "./ambient/boolean.d.ts",
+    "./ambient/function.d.ts",
+    "./ambient/string.d.ts",
+    "./ambient/number.d.ts",
+    "./ambient/iarguments.d.ts",
+    "./ambient/regexp.d.ts",
+    "./ambient/error.d.ts"
+].map(f => path.join(__dirname, f));
+
+var fileString = files.join(" ");
+
+exec('cat ' + fileString + ' > ' + outputFile);

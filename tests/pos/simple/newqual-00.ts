8987eb0d57ecb36ffb2fea951ca1777f09f00b56
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-10-29 10:40:02 -0700

    rejigger qualifier parser to use TS types instead of FP sorts.

--- tests/pos/simple/newqual-00.ts
+++ tests/pos/simple/newqual-00.ts
@@ -1,12 +1,24 @@
-/* qualif Eq10(v:number): v = 10 */
 
+// This works, but what is `int` (in .ts land) ?
+
+/* qualif Eq5(v:int): v = 5 */
+
+// This fails, because currently parsed as `num @(0)` (yikes!)
+
+/* qualif Eq5(v:number): v = 5 */
+
+// GOAL: get this new syntax to work
+
+/*@ qualif Eq5(v:number) { v = 5 } */
+
+
+/*@ foo :: () => number */
 function foo():number{
-  return 20;
+  return 5;
 }
 
-/*@ main :: (x:number) => {v:number | true} */
-function main(x:number):number { 
+/*@ main :: (x:number) => {v:number | v = 10} */
+function main(x:number):number {
   var a = foo();
-  assert(a === 10);
-  return a;
+  return (a + 5);
 }

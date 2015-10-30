d0b6d1365407023ea809e68a256dba0208a7188e
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-10-29 20:46:42 -0700

    pad

--- tests/neg/inclusion/forin-02.ts
+++ tests/neg/inclusion/forin-02.ts
@@ -1,6 +1,5 @@
-
-/*@ qualif Bot(v:a,s:string): hasProperty(v,s) */
-/*@ qualif Bot(v:a,s:string): enumProp(v,s) */
+/*@ qualif HasP(v:string, s:A): hasProperty(v,s) */
+/*@ qualif EnumP(v:string, s:A): enumProp(v,s)    */
 
 /*@ foo :: (o: [Immutable]{ [x:string]: string }) => { MArray<string> | 0 < 1 } */ 
 function foo(o) {

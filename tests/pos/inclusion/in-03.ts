9b258ee41ac6bc167e66642c9c2ea7f8c6b6905e
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2016-02-08 16:48:43 -0800

    in-03.ts needs Math.PI which was removed for compatness

--- tests/pos/inclusion/in-03.ts
+++ tests/pos/inclusion/in-03.ts
@@ -1,7 +1,7 @@
 
 // Predefined objects
-assert("PI" in Math);         // returns true
- 
+// assert("PI" in Math);         // returns true -- PV: removed from prelude for compactness
+
 // // Custom objects
 var mycar = {make: "Honda", model: "Accord", year: 1998};
 assert("make" in mycar);  // returns true

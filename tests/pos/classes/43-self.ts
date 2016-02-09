83261e0661d7d39aea3fd562c4bcc5587dedfaf4
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2016-02-08 23:17:14 -0800

    Done with pos/classes

---
+++ tests/pos/classes/43-self.ts
@@ -0,0 +1,23 @@
+class IdGen<M extends ReadOnly> {
+
+    /*@ @Mutable gen(): number */
+    gen() {
+        return 0;
+    }
+    constructor() { }
+}
+
+class UniqueIdGen<M extends ReadOnly> extends IdGen<M> {
+
+    private next = -1;
+
+    /*@ @Mutable gen(): number */
+    gen() {
+        this.next++;
+        return this.next;
+    }
+
+    constructor() {
+        super();
+    }
+}

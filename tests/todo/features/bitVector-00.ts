77f9de83afbf437ddec6d8479cdd27c1f65ba3d7
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2016-02-12 14:52:35 -0800

    Minor cleanup

--- tests/todo/features/bitVector-00.ts
+++ tests/todo/features/bitVector-00.ts
@@ -1,5 +1,4 @@
 
-
 /*@ predicate IsField(x,f,t)    = keyVal(x,f) & t = t  */
 
 /*@ predicate P_Symbol(X, B, N) = IsField(X, "flags", B) => extends_interface(X, N) */
@@ -11,13 +10,13 @@ enum SymbolFlags {
     Import             = 0x00400000,  // Import
     Instantiated       = 0x00800000,  // Instantiated symbol
     Merged             = 0x01000000,  // Merged symbol (created during program binding)
-    Transient          = 0x02000000,  // Transient symbol (created during type check)   
+    Transient          = 0x02000000,  // Transient symbol (created during type check)
     Prototype          = 0x04000000,  // Symbol for the prototype property (without source code representation)
 }
 
 interface Symbol {
     flags: SymbolFlags;
-    name: string; 
+    name: string;
 }
 
 interface TransientSymbol extends Symbol { }
@@ -32,4 +31,3 @@ function getSymbolLinks(symbol: Symbol): void {
     }
 
 }
-

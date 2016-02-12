642fede053fd227139ead7ae65bbc076a7d222a0
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2016-02-12 14:21:31 -0800

    Pulling in some more tests

--- tests/pos/misc/27-negate.ts
+++ tests/pos/misc/27-negate.ts
@@ -1,11 +1,9 @@
 
 /*@ negate :: (xxx: {number | v > 0} + boolean) =>  { v: number | v < 0 } + boolean */
-
-function negate(xxx):any {
+function negate(xxx): any {
   if (typeof(xxx) === "number") {
     return 0 - xxx;
-  } 
+  }
   return 0 - 1;
-  
-}
 
+}
---
+++ tests/pos/misc/30-negate.ts
@@ -0,0 +1,13 @@
+/*@ negate :: (x: number)  => { number  | v > 0 <=> x < 0   } */
+/*@ negate :: (x: boolean) => { boolean | Prop v <=> not (Prop x) } */
+export function negate(x): any {
+  if (typeof(x) === "number") {
+    return 0 - x;
+  }
+  else {
+    return !x;
+  }
+}
+
+let a: number = negate(10);
+let b: boolean = negate(true);

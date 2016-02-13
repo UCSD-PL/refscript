5ddf531c05b4d9f8bcb5278e4ab6dd3851b7756b
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2016-02-12 22:59:20 -0800

    Fixing test

--- tests/pos/inclusion/02-inclusion.ts
+++ tests/pos/inclusion/02-inclusion.ts
@@ -1,5 +1,5 @@
 
-/*@ d3_vendorSymbol :: (object: {}, name: string) => { boolean | ((Prop v) <=> hasProperty(name, object)) } */
+/*@ d3_vendorSymbol :: (object: {}, name: string) => { boolean | ((Prop v) <=> hasProperty(object, name)) } */
 function d3_vendorSymbol(object:any, name:string) {
     return name in object;
 }

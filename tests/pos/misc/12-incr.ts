c72798fe4a3784ef32133d04078810eb50ee187b
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2016-02-13 21:47:51 -0800

    Adding tests

---
+++ tests/pos/misc/12-incr.ts
@@ -0,0 +1,30 @@
+
+/*@ option --real */
+
+/*@ qualif Add1(v: int, y: int): v = y + 1 */
+
+function incr(x:number): number {
+    return ++x;
+}
+
+/*@ incrBy :: (x:number, k:number) => {number|v = x + k} */
+function incrBy(x:number, k:number):number{
+  return x += k;
+}
+
+/*@ efoo :: (x:number) => {number|v = x + 7} */
+function efoo(x:number):number{
+    return x += 7;
+}
+
+/*@ foo :: (x: number) => { number | v = 3*x + 3} */
+function foo(x:number):number{
+    return x += incr(x++) + incr(x++);
+}
+
+/*@ bar :: (x:number) => {number|v = x + 4} */
+function bar(x:number):number{
+    let z = incrBy(x++, x++);
+    z = incrBy(x++, x++);
+    return x;
+}
---
+++ tests/pos/misc/22-max.ts
@@ -0,0 +1,12 @@
+/*@ max :: (number, number) => number */
+export function max(x:number, y:number):number{ 
+	let r :number= 0;
+	if (x > y) {
+		r = x;
+	} else {
+		r = y;
+	}
+	assert(r >= x);
+	assert(r >= y);
+	return r;
+}
